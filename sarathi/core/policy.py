03dff62d23746288767c86a98309f9f002ae832e
Charmstok <3180292399@qq.com>
2026-04-15 15:58:56 +0800

    refactor: 复原`policy.py`

--- sarathi/core/policy.py
+++ sarathi/core/policy.py
@@ -1,19 +1,16 @@
-import os
 from typing import List
-from abc import ABC, abstractmethod
 
 from sarathi.core.datatypes.sequence import Sequence
 
 
-class Policy(ABC):
+class Policy:
 
-    @abstractmethod
     def get_priority(
         self,
         now: float,
         seq: Sequence,
     ) -> float:
-        pass
+        raise NotImplementedError
 
     def sort_by_priority(
         self,
@@ -26,7 +23,7 @@ class Policy(ABC):
             reverse=True,
         )
 
-# 先来先服务
+
 class FCFS(Policy):
 
     def get_priority(
@@ -36,55 +33,13 @@ class FCFS(Policy):
     ) -> float:
         return now - seq.arrival_time
 
-# 最短prompt优先
-class SPF(Policy):
-
-    def get_priority(
-        self,
-        now: float,
-        seq: Sequence,
-    ) -> float:
-        return -1.0 * seq.get_prompt_len()
-
-# 最短剩余时间优先
-class SRTF(Policy):
-
-    def get_priority(
-        self,
-        now: float,
-        seq: Sequence,
-    ) -> float:
-        return -1.0 * (seq.get_prompt_len() - seq.get_num_prompt_tokens_stage_processed())
-
-# 老化策略
-class AGING(Policy):
-
-    def get_priority(
-        self,
-        now: float,
-        seq: Sequence
-    ) -> float:
-        time_weight = 125.0 * 0.3
-        prompt_weight = -1.0
-
-        return (
-            time_weight * (now - seq.arrival_time)
-            + prompt_weight * (seq.get_prompt_len() - seq.get_num_prompt_tokens_stage_processed())
-        )
 
 class PolicyFactory:
 
     _POLICY_REGISTRY = {
         "fcfs": FCFS,
-        "spf": SPF,
-        "srtf": SRTF,
-        "aging": AGING,
     }
 
     @classmethod
     def get_policy(cls, policy_name: str, **kwargs) -> Policy:
         return cls._POLICY_REGISTRY[policy_name](**kwargs)
-
-    @classmethod
-    def get_available_policies(cls) -> List[str]:
-        return list(cls._POLICY_REGISTRY.keys())
